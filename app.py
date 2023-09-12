942c56c0d3fdf4c55edd3127f181e64dac08826d
jackpot6663 <37533840+jackpot66663@users.noreply.github.com>
2023-09-13 00:16:58 +0800

    login page & mysql setting

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import MySQLdb.cursors
+import re
+import hashlib
+
 app = Flask(__name__)
 
 
-@app.route("/")
+app.secret_key = ''
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '610520Jjkk++'
+app.config['MYSQL_DB'] = 'pythonlogin'
+
+# Intialize MySQL
+mysql = MySQL(app)
+
+
+@app.route('/')
 def home():
-    return 'hello flask'
+    return redirect(url_for('login'))
 
 
-@app.route("/hello")
-def hello():
-    return render_template('hello.html')
-
-
-@app.route("/game")
-def game():
-    return 'game'
+@app.route("/python_web", methods=['GET'])
+def login():
+    return render_template('index.html')
